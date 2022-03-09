a9d93cb99f01862d273373ea6fe0cb161a57c5fa
Yan Gustavo <fekieh35@gmail.com>
2022-03-09 16:00:48 -0300

    exercicios 4.2 quase completos

---
+++ fundamentos/bloco-04 Introducao-a-JAVASCRIPT/exercicio 1.js
@@ -0,0 +1,41 @@
+let a = 5;
+let b = 7;
+
+let Adição = a + b;
+let Subtração = a - b;
+let Multiplicação = a * b;
+let Divisão = a / b;
+let Módulo = a % b;
+
+/* console.log(Adição)
+console.log(Subtração)
+console.log(Multiplicação)
+console.log(Divisão)
+console.log(Módulo) */
+
+// Comparador maior número
+/* let comparamaior;
+
+if (a < b) {
+  comparamaior = b;
+} else {
+  comparamaior = a;
+}
+
+console.log("O maior número é: ", comparamaior); */
+//-----------
+
+let c = 9; //adicionando novo número
+/* let comparatres = a;
+
+if (a < b) {
+  comparatres = b;
+}
+if (comparatres < c) {
+  comparatres = c;
+}
+
+console.log("seu maior número é:", comparatres);
+ */ //comparador de 3 números
+
+ 
